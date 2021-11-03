41d9a4c1d4ca3fc9e5c0507f7e9b2a727b81a48b
da624492 <da624492@gmail.com>
2021-11-03 09:19:10 -0500

    titilos

--- src/main/java/co/gov/sdm/unirExcel/servicio/UtilExcelSDM.java
+++ src/main/java/co/gov/sdm/unirExcel/servicio/UtilExcelSDM.java
@@ -7,10 +7,33 @@ import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class UtilExcelSDM extends UtilExcel {
+	/**
+	 * Extrae el contenido de la fila dada de la hoja dada de un archivo en formato 2021 y lo pega en shSalida
+	 * @return La fila de titulos
+	 * */
+	public static Row copiarTitulos(File archivo, int hoja, int indFilaTitulos, SXSSFWorkbook wbSalida, Sheet shSalida) throws IOException {				
+		FileInputStream fis = new FileInputStream(archivo);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);						
+	    try {
+			XSSFSheet sheet = wb.getSheetAt(hoja);		   
+	    	Row r = sheet.getRow(indFilaTitulos);
+	    	Row rt = copiarFila(r, shSalida, 0);
+	    	rt.createCell( rt.getLastCellNum() ).setCellValue("ARCHIVO ORIGINAL");
+	    	rt.setRowStyle(estilosNegrilla(wbSalida));
+	    	return rt;
+	    } finally {
+	    	if (fis != null)
+	    		fis.close();
+	    	if (wb != null)
+	    		wb.close();
+	    }
+	}
+	
 	/**
 	 * Extrae el contenido de la hoja dada de un archivo en formato 2021 y lo pega en shSalida
 	 * */
@@ -21,7 +44,7 @@ public class UtilExcelSDM extends UtilExcel {
 			XSSFSheet sheet = wb.getSheetAt(hoja);		   
 		    int filas = 0;
 		    String nombArch = archivo.getName();
-		    
+		    		
 		    // procesa todas las filas en la hoja
 		    Iterator<Row> it = sheet.iterator();
 		    while (it.hasNext()) {
--- src/main/java/co/gov/sdm/unirExcel/servicio/UtilExcelWilber.java
+++ src/main/java/co/gov/sdm/unirExcel/servicio/UtilExcelWilber.java
@@ -30,26 +30,47 @@ public class UtilExcelWilber extends UtilExcelSDM {
 			int filasCompiladoDesc = 1;
 			int filasCompiladoSenEle = 1;
 			int filasCompiladoCont = 1;
-			int filasCompiladoContSeg = 1;			
+			int filasCompiladoContSeg = 1;
+			
+			int colArchDem = -1;
+			int colArchDemSeg = -1;
+			int colArchSen = -1;
+			int colArchDesc = -1;
+			int colArchSenEle = -1;
+			int colArchCont = -1;
+			int colArchContSeg = -1;
 			
 			// Itera los archivos en el directorio home
 		    File dir = new File(carpetaEntrada);
 		    File[] archs = dir.listFiles();
+		    boolean primerExcelLeido = false;
+		    
 		    for (int a = 0; a < archs.length; a++) {
 		    	File arch = archs[a];
-		    	if (arch.getName().endsWith(".xlsx")) {		    		
-		    		filasCompiladoDem = extraerFilas2021(arch, 0, 7, shDemarcacion, filasCompiladoDem, 29);
-		    		filasCompiladoDemSeg = extraerFilas2021(arch, 1, 1, shDemarcacionSeg, filasCompiladoDemSeg, 29);
-		    		filasCompiladoSen = extraerFilas2021(arch, 2, 7, shSenalizacion, filasCompiladoSen, 29);
-		    		filasCompiladoDesc = extraerFilas2021(arch, 3, 8, shDescripcionTablero, filasCompiladoDesc, 29);
-		    		filasCompiladoSenEle = extraerFilas2021(arch, 4, 7, shSenElevada, filasCompiladoSenEle, 29);
-		    		filasCompiladoCont = extraerFilas2021(arch, 5, 7, shControl, filasCompiladoCont, 29);
-		    		filasCompiladoContSeg = extraerFilas2021(arch, 6, 1, shControlSegmento, filasCompiladoContSeg, 29);		    				    		
+		    	if (arch.getName().endsWith(".xlsx")) {
+		    		if (!primerExcelLeido) {
+		    			colArchDem = copiarTitulos(arch, 0, 6, wbSalida, shDemarcacion).getLastCellNum()-1;
+		    			colArchDemSeg = copiarTitulos(arch, 1, 0, wbSalida, shDemarcacionSeg).getLastCellNum()-1;
+		    			colArchSen = copiarTitulos(arch, 2, 6, wbSalida, shSenalizacion).getLastCellNum()-1;
+		    			colArchDesc = copiarTitulos(arch, 3, 7, wbSalida, shDescripcionTablero).getLastCellNum()-1;
+		    			colArchSenEle = copiarTitulos(arch, 4, 6, wbSalida, shSenElevada).getLastCellNum()-1;
+		    			colArchCont = copiarTitulos(arch, 5, 6, wbSalida, shControl).getLastCellNum()-1;
+		    			colArchContSeg = copiarTitulos(arch, 6, 0, wbSalida, shControlSegmento).getLastCellNum()-1;
+		    			primerExcelLeido = true;
+		    		}
+		    		
+		    		filasCompiladoDem = extraerFilas2021(arch, 0, 7, shDemarcacion, filasCompiladoDem, colArchDem);
+		    		filasCompiladoDemSeg = extraerFilas2021(arch, 1, 1, shDemarcacionSeg, filasCompiladoDemSeg, colArchDemSeg);
+		    		filasCompiladoSen = extraerFilas2021(arch, 2, 7, shSenalizacion, filasCompiladoSen, colArchSen);
+		    		filasCompiladoDesc = extraerFilas2021(arch, 3, 8, shDescripcionTablero, filasCompiladoDesc, colArchDesc);
+		    		filasCompiladoSenEle = extraerFilas2021(arch, 4, 7, shSenElevada, filasCompiladoSenEle, colArchSenEle);
+		    		filasCompiladoCont = extraerFilas2021(arch, 5, 7, shControl, filasCompiladoCont, colArchCont);
+		    		filasCompiladoContSeg = extraerFilas2021(arch, 6, 1, shControlSegmento, filasCompiladoContSeg, colArchContSeg);
 		    	}
 		    }
 		    
 		    // Finaliza la creacion del archivo de salida		    
-		    SimpleDateFormat sdf = new SimpleDateFormat("dd_MM_yyyy");		    
+		    SimpleDateFormat sdf = new SimpleDateFormat("dd_MM_yyyy");
 		    String nombreSalida = "COMPILADO_"+sdf.format(new Date()) + ".xlsx";
 		    out = new FileOutputStream(carpetaSalida + nombreSalida);
 		    wbSalida.write(out);
